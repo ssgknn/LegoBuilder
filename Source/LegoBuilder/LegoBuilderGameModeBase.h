ba85b4506dfec565a4baa065d0e9361b5ba66941
ssgknn <ssgknn@gmail.com>
2023-11-04 17:36:43 +0100

    Project Created

---
+++ Source/LegoBuilder/LegoBuilderGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LegoBuilderGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEGOBUILDER_API ALegoBuilderGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
